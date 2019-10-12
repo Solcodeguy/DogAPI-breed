5578b896eb22128e445eb87e12d25fd7bca6158d
solcodeguy <smeighn@gmail.com>
2019-10-12 19:02:58 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,72 +1,71 @@
 'use strict';
 
 function inputListener() {
-  $('#dog-form').on('submit', function(event) {
-    $('.dog-image-results').empty();
-    event.preventDefault();
-    let breedValue = $('#dog-input').val();
-    console.log(breedValue);
-    pullDogImages(breedValue);
-  });
+    $('#dog-form').on('submit', function(event) {
+        $('.dog-image-results').empty();
+        event.preventDefault();
+        let breedValue = $('#dog-input').val();
+        console.log(breedValue);
+        pullDogImages(breedValue);
+    });
 }
 
 function pullDogImages(breedValue) {
-  console.log(`This ${breedValue} was passed to the pullDogImages`);
-  fetch(`https://dog.ceo/api/breed/${breedValue}/images/random`)
+    console.log(`This ${breedValue} was passed to the pullDogImages`);
+    fetch(`https://dog.ceo/api/breed/${breedValue}/images/random`)
 
-      .then(response => {
+    .then(response => {
         console.log(response);
         return response.json();
-      })
-      
-      .then(responseJSON => {
-      
-      if (responseJSON.status == "error") {
-        
-        console.log(responseJSON.status);
-        return randomBreed();
+    })
 
-      } else if (responseJSON.status == "success") {
-        
-        console.log(responseJSON.status);
+    .then(responseJSON => {
 
-        return showDogs(responseJSON);
+        if (responseJSON.status == "error") {
 
-      } else {
+            console.log(responseJSON.status);
+            return randomBreed();
 
-        alert("Dead end.");
+        } else if (responseJSON.status == "success") {
 
-      }
-      })
+            console.log(responseJSON.status);
 
-     .catch(error => console.log(error));
+            return showDogs(responseJSON);
+
+        } else {
+
+            alert("Try Again.");
+
+        }
+    })
+
+    .catch(error => console.log(error));
 }
 
 function showDogs(responseJSON) {
-    
-    console.log("showDogs fired.");
+
+    console.log("showDogs.");
     $('.dog-image-results').append(`<img src="${responseJSON.message}">`);
 }
 
 function randomBreed() {
-  console.log("We made it to Other option.");
-  fetch(`https://dog.ceo/api/breeds/image/random/1`)
-    .then(response => response.json())
-    .then(responseJson => {
-         showOther(responseJson);
-      })
-    .catch(error => alert("Something went wrong"));
+    console.log("Other option.");
+    fetch(`https://dog.ceo/api/breeds/image/random/1`)
+        .then(response => response.json())
+        .then(responseJson => {
+            showOther(responseJson);
+        })
+        .catch(error => alert("Something went wrong"));
 }
 
 function showOther(responseJson) {
 
-  $('.result-notes').html(`Oh no! Looks like we hit a snag, and can't find that breed. Here's a pooch we like though!`);
-  
-  return $('.dog-image-results').append(`<img src="${responseJson.message}">`);
-  
+    $('.result-notes').html(`Oh no! hit a snag, and can't find that breed. Here's one we like instead!`);
+
+    return $('.dog-image-results').append(`<img src="${responseJson.message}">`);
+
 }
 
 $(function() {
-  inputListener();
-}
-);
+    inputListener();
+});
